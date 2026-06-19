7d3b9e6e491012b528ef529050faf0aab9fd289f
callitus <ackahfeladan@gmail.com>
2026-06-19 15:26:43 +0400

    build: clean next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Ignores strict TypeScript compilation errors on build
     ignoreBuildErrors: true,
   },
-  // Modern configuration parameter for Next.js to ignore ESLint blocks on build
-  eslint: {
-    ignoreDuringBuilds: true,
-  }
 };
 
 export default nextConfig;
